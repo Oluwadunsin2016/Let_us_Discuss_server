bda4bc6672b0b9b62d08064705a150a0fa753c24
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-06-26 23:33:02 +0100

    Updated

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const URI = process.env.URI;
 app.use(cors());
 app.use(express.json({limit:"50mb"}));
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}));
+app.get('/', (req, res)=>{
+res.json({message:'The page is working fine'})
+})
 app.use("/api/auth", userRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/group", groupRoute);
