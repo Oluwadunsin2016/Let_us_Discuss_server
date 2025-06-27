51a58890934e82c3dfc1c929a0b0fb95d02d051b
Oluwadunsin <oluwadunsin2016@gmail.com>
2025-06-27 10:37:49 +0100

    Updated index file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json({limit:"50mb"}));
 app.use(bodyParser.urlencoded({extended:true, limit:"50mb"}));
 app.get('/', (req, res)=>{
-res.json({message:'The page is working fine'})
+res.json({message:'Welcome to Let us Discuss...'})
 })
 app.use("/api/auth", userRoute);
 app.use("/api/message", messageRoute);
